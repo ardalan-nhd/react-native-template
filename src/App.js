7cabb7c04b1edc079d142a6e05c401564da7ded6
ardalan_nhd <ardalan.nhd@gmail.com>
2022-01-01 21:40:15 +0330

    creating template(splash screen)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import 'react-native-gesture-handler'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <View>
       <Text></Text>
